b490ae3b0343ab5f10a3d977bb48f30add216ca9
ivanvikvik <ivanvikvik@gmail.com>
2025-05-10 20:08:39 +0300

    Almost solve Task05

--- Task02/logic.cpp
+++ Task02/logic.cpp
@@ -6,6 +6,27 @@
 // расположенных на главной и побочной диагоналях.
 #include "logic.h"
 
+// O(N)
 int sum_main_and_second_diagonales_elements(int** matrix, int n) {
-	return 0;
+	if (matrix == nullptr || n <= 0) {
+		return 0;
+	}
+
+	int sum = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		sum += matrix[i][i];
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		sum += matrix[i][n - 1 - i];
+	}
+
+	if (n % 2 == 1) {
+		sum -= matrix[n / 2][n / 2];
+	}
+	   	 
+	return sum;
 }
